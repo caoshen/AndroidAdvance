0007e54d0a9dec71aa7d81a2c744830fc27dc153
caoshen <aradora@foxmail.com>
2020-09-01 12:56:23 +0800

    增加predex transform

--- javassist-gradle-plugin/src/main/groovy/com/example/javassist/PreDexTransform.groovy
+++ javassist-gradle-plugin/src/main/groovy/com/example/javassist/PreDexTransform.groovy
@@ -1,6 +1,9 @@
 package com.example.javassist
 
 import com.android.build.api.transform.Context
+import com.android.build.api.transform.DirectoryInput
+import com.android.build.api.transform.Format
+import com.android.build.api.transform.JarInput
 import com.android.build.api.transform.QualifiedContent
 import com.android.build.api.transform.Transform
 import com.android.build.api.transform.TransformException
@@ -8,6 +11,7 @@ import com.android.build.api.transform.TransformInput
 import com.android.build.api.transform.TransformInvocation
 import com.android.build.api.transform.TransformOutputProvider
 import com.android.build.gradle.internal.pipeline.TransformManager
+import com.android.utils.FileUtils
 import org.gradle.api.Project
 
 /**
@@ -23,6 +27,7 @@ class PreDexTransform extends Transform {
     /**
      * Transform 在 Task 列表的名字
      * TransformClassesWithPreDexForXXXX
+     * transformClassesWithPreDexForDebug
      *
      * @return
      */
@@ -54,14 +59,26 @@ class PreDexTransform extends Transform {
             TransformException, InterruptedException {
         log("transform >>>>>")
         inputs.each { TransformInput input ->
-            // Transform 输入是目录
-            input.directoryInputs.each {
-
+            // Transform 输入是目录，是自己项目的 classes
+            input.directoryInputs.each { DirectoryInput directoryInput ->
+                log("directoryInput name = " + directoryInput.name + ", path = " + directoryInput.file.absolutePath)
+                def dest = outputProvider.getContentLocation(directoryInput.name, directoryInput.contentTypes,
+                    directoryInput.scopes, Format.DIRECTORY)
+                FileUtils.copyDirectory(directoryInput.file, dest)
             }
 
-            // Transform 输入是 jar
-            input.jarInputs.each {
-
+            // Transform 输入是 jar，是项目依赖的 jar
+            input.jarInputs.each { JarInput jarInput ->
+                log("jarInput name = " + jarInput.name + ", path = " + jarInput.file.absolutePath)
+                // 重命名输出文件（同目录执行 copyFile 操作会发生冲突）
+                def jarName = jarInput.name
+                def md5Name = jarInput.file.hashCode()
+                if (jarName.endsWith(".jar")) {
+                    jarName = jarName.substring(0, jarName.length() - 4)
+                }
+                def dest = outputProvider.getContentLocation(jarName + md5Name, jarInput.contentTypes,
+                    jarInput.scopes, Format.JAR)
+                FileUtils.copyFile(jarInput.file, dest)
             }
 
         }
