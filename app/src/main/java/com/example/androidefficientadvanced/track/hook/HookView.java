32cb3f2ad3d1ae9009f079bdda2fc4857748d6e0
caoshen <aradora@foxmail.com>
2020-10-09 20:00:08 +0800

    track click and exposure

---
+++ app/src/main/java/com/example/androidefficientadvanced/track/hook/HookView.java
@@ -0,0 +1,35 @@
+package com.example.androidefficientadvanced.track.hook;
+
+import android.view.View;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * @author caoshen
+ * @date 2020/10/9
+ */
+public class HookView {
+    public Method mHookMethod;
+
+    public Field mHookField;
+
+    public HookView(View view) {
+        try {
+            Class<?> viewClass = Class.forName("android.view.View");
+            mHookMethod = viewClass.getDeclaredMethod("getListenerInfo");
+            mHookMethod.setAccessible(true);
+
+            Class<?> listenerInfoClass = Class.forName("android.view.View$ListenerInfo");
+            mHookField = listenerInfoClass.getDeclaredField("mOnClickListener");
+            mHookField.setAccessible(true);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/androidefficientadvanced/track/hook/ProxyManager.java
@@ -0,0 +1,32 @@
+package com.example.androidefficientadvanced.track.hook;
+
+import android.view.View;
+
+/**
+ * @author caoshen
+ * @date 2020/10/9
+ */
+public class ProxyManager {
+
+    public static void sendLog(View view) {
+
+    }
+
+    public static class ProxyListener implements View.OnClickListener {
+
+        View.OnClickListener mOriginalListener;
+
+        public ProxyListener(View.OnClickListener listener) {
+            mOriginalListener = listener;
+        }
+
+        @Override
+        public void onClick(View v) {
+            // send log
+            sendLog(v);
+            if (mOriginalListener != null) {
+                mOriginalListener.onClick(v);
+            }
+        }
+    }
+}
