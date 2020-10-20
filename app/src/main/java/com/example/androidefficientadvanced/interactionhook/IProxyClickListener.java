a7982597a864065b743c4f00907602b8f99af757
caoshen <aradora@foxmail.com>
2020-10-20 11:12:28 +0800

    add proxy click

---
+++ app/src/main/java/com/example/androidefficientadvanced/interactionhook/IProxyClickListener.java
@@ -0,0 +1,35 @@
+package com.example.androidefficientadvanced.interactionhook;
+
+import android.view.View;
+
+/**
+ * @author caoshen
+ * @date 2020/10/10
+ */
+public interface IProxyClickListener {
+
+    boolean onProxyClick(WrapClickListener wrap, View view);
+
+    class WrapClickListener implements View.OnClickListener {
+
+        IProxyClickListener mProxyListener;
+
+        View.OnClickListener mBaseListener;
+
+        public WrapClickListener(IProxyClickListener proxyListener, View.OnClickListener baseListener) {
+            mProxyListener = proxyListener;
+            mBaseListener = baseListener;
+        }
+
+        @Override
+        public void onClick(View v) {
+            boolean handled = false;
+            if (mProxyListener != null) {
+                handled = mProxyListener.onProxyClick(this, v);
+            }
+            if (!handled && mBaseListener != null) {
+                mBaseListener.onClick(v);
+            }
+        }
+    }
+}
