35b0a3136d0559dbbca84eff43548d2d886c7fec
caoshen <aradora@foxmail.com>
2022-05-24 19:48:52 +0800

    add matrix

---
+++ matrix/src/main/java/io/github/caoshen/androidadvance/matrix/MainActivity.java
@@ -0,0 +1,21 @@
+package io.github.caoshen.androidadvance.matrix;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
---
+++ matrix/src/main/java/io/github/caoshen/androidadvance/matrix/trace/TestTraceMainActivity.java
@@ -0,0 +1,6 @@
+package io.github.caoshen.androidadvance.matrix.trace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TestTraceMainActivity extends AppCompatActivity {
+}
