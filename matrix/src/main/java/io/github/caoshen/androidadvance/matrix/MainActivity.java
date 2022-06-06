9fac33403f6ab8dc9e731645919f5f966136c309
caoshen <aradora@foxmail.com>
2022-06-06 14:57:28 +0800

    matrix trace

--- matrix/src/main/java/io/github/caoshen/androidadvance/matrix/MainActivity.java
+++ matrix/src/main/java/io/github/caoshen/androidadvance/matrix/MainActivity.java
@@ -1,9 +1,14 @@
 package io.github.caoshen.androidadvance.matrix;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import io.github.caoshen.androidadvance.matrix.trace.TestTraceMainActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -12,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button testTrace = findViewById(R.id.test_trace);
+        testTrace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TestTraceMainActivity.class));
+            }
+        });
     }
 
     @Override
--- matrix/src/main/java/io/github/caoshen/androidadvance/matrix/trace/TestTraceMainActivity.java
+++ matrix/src/main/java/io/github/caoshen/androidadvance/matrix/trace/TestTraceMainActivity.java
@@ -1,6 +1,14 @@
 package io.github.caoshen.androidadvance.matrix.trace;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TestTraceMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
